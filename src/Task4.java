afc85418b1482548f77cf41d5937852c5efdc948
roman <roman-1901@mail.ru>
2022-10-27 14:01:12 +0300

    Homework5

--- src/Task4.java
+++ src/Task4.java
@@ -1,14 +1,19 @@
 public class Task4 {
     public static void main(String[] args) {
-        int deliveryDistance = 60;
+        int deliveryDistance = 95;
         int interval = 40;
+        int firstDayDistance = 20;
         int countDay = 1;
-        if (deliveryDistance % interval < 20) {
-            countDay += deliveryDistance / interval;
-            System.out.println("Потребуется дней: " + countDay);
-        }else {
-            countDay += deliveryDistance / interval + 1;
-            System.out.println("Потребуется дней: " + countDay);
+        if (interval < firstDayDistance) {
+            System.out.println("Интервал не может быть меньше допустимого расстояния первого дня");
+        } else {
+            if (deliveryDistance % interval < firstDayDistance) {
+                countDay = countDay + deliveryDistance / interval;
+                System.out.println("Потребуется дней: " + countDay);
+            } else {
+                countDay = countDay + (deliveryDistance / interval + 1);
+                System.out.println("Потребуется дней: " + countDay);
+            }
         }
     }
 }
