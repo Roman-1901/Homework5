09c378ad6b51f71140196363f12e617ad774f61a
roman <roman-1901@mail.ru>
2022-10-27 21:11:07 +0300

    Homework5

--- src/Task4.java
+++ src/Task4.java
@@ -1,7 +1,7 @@
 public class Task4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) {// В данном коде число дней не ограниченно. Рассматривается любая дистанция с учетом определенного интервала
         int deliveryDistance = 95;
-        int interval = 40;
+        int interval = 40; //Так как в задаче учитывается интервал расстояния от 20 до 60, от 60 до 100, а это получается 40, то выделена отдельная переменная с данным значением
         int firstDayDistance = 20;
         int countDay = 1;
         if (interval < firstDayDistance) {
