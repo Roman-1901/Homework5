e430cc651f1fc056ce14601bcf5ba882b4d93806
roman <roman-1901@mail.ru>
2022-10-29 10:32:34 +0300

    test

--- src/test.java
+++ src/test.java
@@ -1,23 +1,25 @@
 public class test {
     public static void main(String[] args) {
         int age = 25;
-        int salary = 90_000;
-        int wantedSum = 500_000;
+        double salary = 79251;
+        double wantedSum = 330_000;
 
-        float rate = 10f;
+        double rate = 10;
         String output;
 
-        if (age < 23) {
-            rate = rate + 1;
-        } else if (age < 30 && age >= 23) {
-            rate = (float) (rate + 0.5);
-        }
         if (salary > 80_000) {
-            rate = (float) (rate - 0.7);
+            rate = (rate - 0.7);
+        } else {
+            if (age < 23) {
+                rate = rate + 1;
+            } else if (age < 30) {
+                rate = (rate + 0.5);
+            }
         }
 
+
         double monthlyPayment = ((wantedSum / 100 * rate) / 12) + wantedSum / 12;
-        int maxMonthlyPayment = salary / 2;
+        double maxMonthlyPayment = salary / 2;
         if (maxMonthlyPayment >= monthlyPayment) {
             output = "Одобрено";
         } else {
@@ -25,6 +27,6 @@ public class test {
         }
         System.out.println("Максимальный платеж при ЗП " + salary + " равен " + maxMonthlyPayment + " рублей.");
 
-        System.out.println("Платеж по кредиту " + String.format("%.2f", monthlyPayment)  + " рублей. " + output + ".");
+        System.out.println("Платеж по кредиту " + String.format("%.2f", monthlyPayment) + " рублей. " + output + ".");
     }
 }
